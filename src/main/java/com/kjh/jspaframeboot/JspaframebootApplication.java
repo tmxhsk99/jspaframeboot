0a0063fd18220febbe97ffb0c72da432ba0bef92
tmxhsk99 <tmxhsk99@naver.com>
2022-09-13 20:31:19 +0900

    Initial commit

---
+++ src/main/java/com/kjh/jspaframeboot/JspaframebootApplication.java
@@ -0,0 +1,13 @@
+package com.kjh.jspaframeboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspaframebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspaframebootApplication.class, args);
+	}
+
+}
