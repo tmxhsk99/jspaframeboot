c9b740a8acf88dae898efc24a5575762c17aa934
kimjuhyeon <tmxhsk99@naver.com>
2022-09-20 04:55:03 +0900

    feat : EceptionHandler 로직추가

--- src/main/java/com/kjh/jspaframeboot/controller/ExceptionController.java
+++ src/main/java/com/kjh/jspaframeboot/controller/ExceptionController.java
@@ -2,10 +2,15 @@ package com.kjh.jspaframeboot.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @ControllerAdvice
@@ -13,8 +18,15 @@ public class ExceptionController {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public void InvalidRequestHandler(MethodArgumentNotValidException e) {
+    @ResponseBody
+    public Map<String,String> InvalidRequestHandler(MethodArgumentNotValidException e) {
         //컨트롤러 매개변수에 RequestResult 가 없어야 Controller Advice로직을 탄다...
+        FieldError fieldError = e.getFieldError();
+        String field = fieldError.getField();
+        String defaultMessage = fieldError.getDefaultMessage();
 
+        HashMap<String, String> response = new HashMap<>();
+        response.put(field,defaultMessage);
+        return response;
     }
 }
