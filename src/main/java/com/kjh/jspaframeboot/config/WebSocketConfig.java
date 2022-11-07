43e07c42b2b63cf4f1ebf2daa6f3a3cfcfca1bbc
kimjuhyeon <tmxhsk99@naver.com>
2022-11-08 08:42:26 +0900

    feat : 웹소켓 설정 파일 클래스 추가

---
+++ src/main/java/com/kjh/jspaframeboot/config/WebSocketConfig.java
@@ -0,0 +1,11 @@
+package com.kjh.jspaframeboot.config;
+
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        
+    }
+}
