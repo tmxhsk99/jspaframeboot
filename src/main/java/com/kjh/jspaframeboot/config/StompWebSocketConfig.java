b88ebe5641b899fb7a25bd6a17414c174c61029c
kimjuhyeon <tmxhsk99@naver.com>
2022-11-16 17:31:27 +0900

    feat : Stomp 웹소켓 설정 파일 클래스 추가

---
+++ src/main/java/com/kjh/jspaframeboot/config/StompWebSocketConfig.java
@@ -0,0 +1,17 @@
+package com.kjh.jspaframeboot.config;
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+    }
+}
---
+++ src/main/java/com/kjh/jspaframeboot/controller/WebSocketController.java
@@ -0,0 +1,7 @@
+package com.kjh.jspaframeboot.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebSocketController {
+}
