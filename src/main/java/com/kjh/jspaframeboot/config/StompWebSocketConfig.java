f18f77331883e50ceaa080167d25ca7f7e9551c8
kimjuhyeon <tmxhsk99@naver.com>
2022-11-17 16:47:05 +0900

    feat : Stomp 웹소켓 설정 endPoint 및 MessageBroker설정 추가

--- src/main/java/com/kjh/jspaframeboot/config/StompWebSocketConfig.java
+++ src/main/java/com/kjh/jspaframeboot/config/StompWebSocketConfig.java
@@ -1,17 +1,23 @@
 package com.kjh.jspaframeboot.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@EnableWebSocketMessageBroker
+@Configuration
 public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        
+        registry.addEndpoint("/dashboard").setAllowedOrigins("*").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+        registry.setApplicationDestinationPrefixes("/publish");
+        registry.enableSimpleBroker("/subscribe");
+
     }
 }
